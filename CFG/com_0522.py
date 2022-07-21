e652871e427cb4ea1605f90cf0e6eff2b6463a5a
Lingda Li <lildmh@gmail.com>
2022-07-21 11:26:20 -0400

    Add sim benchmarks to com_0522

--- CFG/com_0522.py
+++ CFG/com_0522.py
@@ -10,12 +10,20 @@ datasets = [
   (data_set_dir + '502.gcc_r.in.mmap.norm', 16220981, 13602728)
 ]
 
+sim_datasets = [
+  (data_set_dir + '508.namd_r.in.mmap.norm', 1105247403, 1105245197),
+  (data_set_dir + '500.perlbench_r.in.mmap.norm', 1184866587, 1184864563)
+  #(data_set_dir + '507.cactuBSSN_r.in.mmap.norm', 1035397081, 24818299),
+  #(data_set_dir + '502.gcc_r.in.mmap.norm', 16220981, 13602728)
+]
+
 def get_out_name(name):
   return name.replace("in.mmap.norm", "out.mmap")
 
 feature_format = np.float32
 target_format = np.int16
 # total batch number is 2328530787 / 4096 = 568488.96
+# training set size = 524288 * 4096 = 2147483648
 testbatchnum = 557056
 testbatchsize = 1024
 validbatchnum = 524288
@@ -29,5 +37,5 @@ valid_end = (validbatchnum + validbatchsize) * ori_batch_size
 
 input_length = 51
 tgt_length = 16
-cfg_num = 2
+cfg_num = 7
 seq_length = 256
