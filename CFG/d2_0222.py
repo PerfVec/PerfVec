77cc51458f868ad38e4fa4777b297e2cef6c70d8
Lingda Li <lildmh@gmail.com>
2022-07-21 11:27:38 -0400

    Add sim benchmarks to d2_0222

--- CFG/d2_0222.py
+++ CFG/d2_0222.py
@@ -10,15 +10,23 @@ datasets = [
   (data_set_dir + '502.gcc_r.inst.mmap.norm', 16220268)
 ]
 
+sim_datasets = [
+  (data_set_dir + '507.cactuBSSN_r.inst.mmap.norm', 1035389426),
+  (data_set_dir + '508.namd_r.inst.mmap.norm', 1105244743),
+  (data_set_dir + '500.perlbench_r.inst.mmap.norm', 1186655322),
+  (data_set_dir + '502.gcc_r.inst.mmap.norm', 16220268)
+]
+
 data_item_format = np.float32
 # total batch number is 816,286.56
 testbatchnum = 760000
 validbatchnum = 720000
 validbatchsize = 8192
+testbatchsize = 128
 
 ori_batch_size = 4096
 test_start = testbatchnum * ori_batch_size
-test_end = (testbatchnum + 1) * ori_batch_size
+test_end = (testbatchnum + testbatchsize) * ori_batch_size
 valid_start = validbatchnum * ori_batch_size
 valid_end = (validbatchnum + validbatchsize) * ori_batch_size
 
