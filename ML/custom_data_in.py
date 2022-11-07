15e2d3e9aefa0135fada4d4b91456043cfc25d0f
Lingda Li <lildmh@gmail.com>
2022-11-07 07:12:17 -0800

    Enable config as an init parameter for datasets

--- ML/custom_data_in.py
+++ ML/custom_data_in.py
@@ -2,25 +2,30 @@ import os
 import torch
 import numpy as np
 from torch.utils.data import Dataset
-from CFG import *
 
 
-assert 'tgt_length' not in globals()
 mm_batch_size = 512
 
 class MemMappedBatchDataset(Dataset):
 
-    def __init__(self, cfgs, start, end):
-        assert len(cfgs) == 2
-        file_name = cfgs[0]
-        in_size = cfgs[1]
-        self.in_arr = np.memmap(file_name, dtype=feature_format, mode='r',
-                                shape=(in_size, input_length))
+    def __init__(self, cfg, paras, start, end):
+        assert len(paras) == 2
+        file_name = paras[0]
+        in_size = paras[1]
+        self.in_arr = np.memmap(file_name, dtype=cfg.feature_format, mode='r',
+                                shape=(in_size, cfg.input_length))
         self.batchsize = mm_batch_size
-        if end < start or end * self.batchsize > in_size:
+        if end < start:
             raise AttributeError("End is illegal.")
+        elif end * self.batchsize > in_size:
+            print("Use the maximum size of", in_size // self.batchsize,
+                  "instead of", end)
+            end = in_size // self.batchsize
+        self.seq_length = cfg.seq_length
+        self.input_length = cfg.input_length
         self.start = start
         self.size = end - start
+        print("Open %s (%d %d %d)" % (file_name, start, end, self.size), flush=True)
 
     def __len__(self):
         return self.size
@@ -31,24 +36,24 @@ class MemMappedBatchDataset(Dataset):
 
         idx += self.start
         idx *= self.batchsize
-        if idx < seq_length:
-            x = np.zeros((seq_length+self.batchsize-1, input_length))
-            x[seq_length-(idx+1):seq_length+self.batchsize-1, :] = np.copy(self.in_arr[0:idx+self.batchsize, :])
+        if idx < self.seq_length:
+            x = np.zeros((self.seq_length+self.batchsize-1, self.input_length))
+            x[self.seq_length-(idx+1):self.seq_length+self.batchsize-1, :] = np.copy(self.in_arr[0:idx+self.batchsize, :])
         else:
-            x = np.copy(self.in_arr[idx+1-seq_length:idx+self.batchsize, :])
+            x = np.copy(self.in_arr[idx+1-self.seq_length:idx+self.batchsize, :])
         x = torch.from_numpy(x.astype('f'))
-        y = torch.zeros(self.batchsiz, 1)
+        y = torch.zeros(self.batchsize, 1)
         return x, y
 
 
 class CombinedMMBDataset(Dataset):
 
-    def __init__(self, file_num, start, end):
-        if file_num > len(datasets):
+    def __init__(self, cfg, file_num, start, end):
+        if file_num > len(cfg.datasets):
             raise AttributeError("Require more files than that exist.")
         total_size = 0
         for i in range(file_num):
-            total_size += datasets[i][1]
+            total_size += cfg.datasets[i][1]
         if end < start or end > total_size:
             raise AttributeError("End is illegal.")
         if start % mm_batch_size != 0 or end % mm_batch_size != 0:
@@ -67,18 +72,16 @@ class CombinedMMBDataset(Dataset):
         cum_start = 0
         cum_size = 0
         for i in range(file_num-1):
-            self.starts.append(int(datasets[i][1] * (start / total_size) / mm_batch_size))
-            self.mm_sizes.append(int(datasets[i][1] * frac / mm_batch_size))
-            print('Open', datasets[i][0], '(%d %d)' % (self.starts[i], self.mm_sizes[i]))
-            self.mm_sets.append(MemMappedBatchDataset(datasets[i],
+            self.starts.append(int(cfg.datasets[i][1] * (start / total_size) / mm_batch_size))
+            self.mm_sizes.append(int(cfg.datasets[i][1] * frac / mm_batch_size))
+            self.mm_sets.append(MemMappedBatchDataset(cfg.datasets[i],
                                 self.starts[i], self.starts[i] + self.mm_sizes[i]))
             cum_start += self.starts[i]
             cum_size += self.mm_sizes[i]
             self.bounds.append(cum_size)
         self.starts.append(start - cum_start)
         self.mm_sizes.append(self.size - cum_size)
-        print('Open', datasets[file_num-1][0], '(%d %d)' % (self.starts[file_num-1], self.mm_sizes[file_num-1]))
-        self.mm_sets.append(MemMappedBatchDataset(datasets[file_num-1],
+        self.mm_sets.append(MemMappedBatchDataset(cfg.datasets[file_num-1],
                             self.starts[file_num-1], self.starts[file_num-1] + self.mm_sizes[file_num-1]))
         self.bounds.append(self.size)
 
