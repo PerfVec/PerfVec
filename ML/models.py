cb73d537044a3f55201205e38d617997decfb780
Lingda Li <lildmh@gmail.com>
2024-01-30 12:49:26 -0500

    Add an instruction embedding layer InsEm

--- ML/models.py
+++ ML/models.py
@@ -148,6 +148,86 @@ class InsLSTMDSE(InsLSTM):
     return x
 
 
+class InsEm(nn.Module):
+  def __init__(self, cfg, nop=1, nmem=0, nctrl=0, nr=0):
+    super(InsEm, self).__init__()
+
+    assert nop > 0
+    self.op_embed = nn.Embedding(50, nop)
+    self.nin = cfg.input_length - 1 + nop
+    self.nmem = nmem
+    if nmem > 0:
+      self.mem_embed = nn.Embedding(256, nmem)
+      self.nin += nmem - 7
+    self.nctrl = nctrl
+    if nctrl > 0:
+      self.ctrl_embed = nn.Embedding(512, nctrl)
+      self.nin += nctrl - 9
+    self.nr = nr
+    if nr > 0:
+      self.reg_embed = nn.Embedding(1024, nr)
+      self.nin += (nr - 2) * 14
+
+    # Load normalization factors.
+    stats = np.load(cfg.data_set_dir + "stats.npz")
+    mean = stats['mean']
+    std = stats['std']
+    std[std == 0.0] = 1.0
+    mean = torch.from_numpy(mean.astype('f'))
+    std = torch.from_numpy(std.astype('f'))
+    self.mean = nn.Parameter(mean)
+    self.std = nn.Parameter(std)
+    self.mean.requires_grad = False
+    self.std.requires_grad = False
+
+  def forward(self, x):
+    ori_x = x * self.std + self.mean
+    ori_x = ori_x.long()
+    embeddings = self.op_embed(ori_x[:, :, 1])
+    if self.nmem > 0:
+      mem_idx = ori_x[:, :, 0]
+      mem_idx = torch.bitwise_left_shift(mem_idx, 1) + ori_x[:, :, 2]
+      mem_idx = torch.bitwise_left_shift(mem_idx, 1) + ori_x[:, :, 3]
+      mem_idx = torch.bitwise_left_shift(mem_idx, 2) + ori_x[:, :, 11]
+      mem_idx = torch.bitwise_left_shift(mem_idx, 1) + ori_x[:, :, 12]
+      mem_idx = torch.bitwise_left_shift(mem_idx, 1) + ori_x[:, :, 13]
+      mem_idx = torch.bitwise_left_shift(mem_idx, 1) + ori_x[:, :, 19]
+      memem = self.mem_embed(mem_idx)
+      embeddings = torch.cat((embeddings, memem), 2)
+    if self.nctrl > 0:
+      ctrl_idx = ori_x[:, :, 4]
+      ctrl_idx = torch.bitwise_left_shift(ctrl_idx, 1) + ori_x[:, :, 5]
+      ctrl_idx = torch.bitwise_left_shift(ctrl_idx, 1) + ori_x[:, :, 6]
+      ctrl_idx = torch.bitwise_left_shift(ctrl_idx, 1) + ori_x[:, :, 7]
+      ctrl_idx = torch.bitwise_left_shift(ctrl_idx, 1) + ori_x[:, :, 8]
+      ctrl_idx = torch.bitwise_left_shift(ctrl_idx, 1) + ori_x[:, :, 9]
+      ctrl_idx = torch.bitwise_left_shift(ctrl_idx, 1) + ori_x[:, :, 10]
+      ctrl_idx = torch.bitwise_left_shift(ctrl_idx, 1) + ori_x[:, :, 14]
+      ctrl_idx = torch.bitwise_left_shift(ctrl_idx, 1) + ori_x[:, :, 15]
+      ctrlem = self.ctrl_embed(ctrl_idx)
+      embeddings = torch.cat((embeddings, ctrlem), 2)
+    if self.nr > 0:
+      regs = ori_x[:, :, 23:].view(-1, seq_length, 14, 2)
+      reg_idx = 50 * regs[:, :, :, 0] + regs[:, :, :, 1]
+      regem = self.reg_embed(reg_idx).view(-1, seq_length, 14 * self.nr)
+      embeddings = torch.cat((embeddings, regem), 2)
+
+    # Combine the rest input.
+    if self.nmem > 0 and self.nctrl > 0:
+      rest = torch.cat((x[:, :, 16:19], x[:, :, 20:23]), 2)
+    elif self.nmem > 0:
+      rest = torch.cat((x[:, :, 4:11], x[:, :, 14:19], x[:, :, 20:23]), 2)
+    elif self.nctrl > 0:
+      rest = torch.cat((x[:, :, 0:1], x[:, :, 2:4], x[:, :, 11:14], x[:, :, 16:23]), 2)
+    else:
+      rest = torch.cat((x[:, :, 0:1], x[:, :, 2:23]), 2)
+    if self.nr == 0:
+      rest = torch.cat((rest, x[:, :, 23:]), 2)
+
+    x = torch.cat((embeddings, rest), 2)
+    return x
+
+
 class SeqEmLSTM(nn.Module):
   def __init__(self, nhidden, nlayers, narchs=1, nop=1, nmem=0, nctrl=0, nr=0, gru=False, bi=False, bias=True):
     super(SeqEmLSTM, self).__init__()
